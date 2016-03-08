efcd7f50f885b3a25314b6e479c8b02b23e0fe2d
Justin Walsh <contact.me@thejustinwalsh.com>
2016-03-08 09:42:55 -0700

    API Key
    Updated API key for referer locked API

--- js/next-meeting.js
+++ js/next-meeting.js
@@ -4,7 +4,7 @@
   var meetupEventsUrl = "https://api.meetup.com/Phoenix-Unreal-Engine-Developers/events?photo-host=public&page=1&sig_id=198889890&status=upcoming%2Cpast&sig=3580ea6dcb9a610b920a52050a114a8b585065e9";
 
   function populateMap(data) {
-    var API_KEY = 'AIzaSyCfFdwX-nhamAMGQX7YHJHe2Rpu6A4OGcw';
+    var API_KEY = 'AIzaSyCJZJPBFj_dGqWruxfkQFEEis8jTf0eVNE';
     var LOCATION = data.venue.address_1 + "," + data.venue.city + " " + data.venue.state;
     $('#meetup-map')[0].src = "https://www.google.com/maps/embed/v1/place?key="+API_KEY+"&q="+encodeURIComponent(LOCATION);
 
